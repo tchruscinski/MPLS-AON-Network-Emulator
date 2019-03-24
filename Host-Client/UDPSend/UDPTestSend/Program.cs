69f9bbb231cadfd772d0fd76d3702bf182d372ca
Mateusz Szewczyk <szewczykmateusz97@gmail.com>
2019-03-24 12:45:36 +0100

    UDPSend i UDPListener test

---
+++ Host-Client/UDPSend/UDPTestSend/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPTestSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //address family okresla schemat adresowania
+            //internetwork = adres ipv4
+            //dgram czyli socket datagramowy czyli udp
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress broadcast = IPAddress.Parse("192.168.1.255");
+            
+            String text = "testtestest";
+            byte[] sendbuf = Encoding.ASCII.GetBytes(text);
+            IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+            s.SendTo(sendbuf, ep);
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+    }
+}
