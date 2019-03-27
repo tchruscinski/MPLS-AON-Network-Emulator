d3ba2db26f1e9da38630b35050e9942d5de99968
Mateusz Szewczyk <szewczykmateusz97@gmail.com>
2019-03-27 20:10:58 +0100

    Added communication between host and cloud launched by NetworkLauncher class

---
+++ test270319/Host/MyHost/MyHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPTestSend
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Router router = new Router(11000, "192.168.1.255");
+            router.SendMessage();
+        }
+    }
+}
+
