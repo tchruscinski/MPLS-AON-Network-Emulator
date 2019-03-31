02f6a14f6bfe1f9a7ba3ff0cfcb805711c6f1ddb
mkowalc <kowalczyk12091996@gmail.com>
2019-03-31 23:04:58 +0200

    Na razie jakis przykladowy kod socketa UDP

---
+++ Connection Cloud/ConnectionCloud/ConnectionCloud/UsageExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConnectionCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("127.0.0.1", 27000);
+
+            UDPSocket c = new UDPSocket();
+            c.Client("127.0.0.1", 27000);
+            c.Send("TEST!");
+
+            Console.ReadKey();
+        }
+    }
+}
