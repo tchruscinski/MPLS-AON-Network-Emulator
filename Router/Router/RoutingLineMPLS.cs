e621fe7e8bb83a10e36584cdb9853dee6f255136
Mateusz Szewczyk <szewczykmateusz97@gmail.com>
2019-04-07 01:11:49 +0200

    MPLS cd

---
+++ Router/Router/RoutingLineMPLS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router
+{
+    /*
+     * Klasa reprezentuje wiersz tablicy FIB-MPLS routera
+     */
+    class RoutingLineMPLS
+    {
+        int _label; //etykieta, ktora zostanie nalozona na pakiet przy wysylaniu
+        int _port; //port, ktorym pakiet zostanie wyslany
+    }
+}
