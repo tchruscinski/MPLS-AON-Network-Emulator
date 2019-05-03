ea9fc74c34679f5e8802de695f5388824a234651
Mikolaj Kowalczyk <kmpl@protonmail.com>
2019-05-03 22:31:08 +0200

    test host

--- Host/MyMain.cs
+++ Host/MyMain.cs
@@ -11,82 +11,93 @@ namespace Host
         public static void Main(string[] args)
         {
 
-            //if (args.Length != 0)
-            //{
-            //    Host host = new Host(args[0]);
-            //    host.ManagementRequest();
-            //    Console.ReadLine();
-            //    if (host.getName().Equals("Host1")) host.SendPacket("Host2", "test test test tset");
-            //    Console.ReadLine();
-            //    return;
-            //}
-            Host host1 = new Host("Host1");
-            host1.ManagementRequest();
-            host1.ShowRoutingLines();
-
-            Host host2 = new Host("Host2");
-            host2.ManagementRequest();
-            host2.ShowRoutingLines();
-            string command;
-
-            CommandLineInterface cli = new CommandLineInterface();
-
-            while (true)
+            if (args.Length != 0)
             {
-                Console.Write("HostCLI# ");
-                command = Console.ReadLine();
-                cli.RunCommand(command);
+                Host host = new Host(args[0]);
+                host.ManagementRequest();
+                string command;
+
+                CommandLineInterface cli = new CommandLineInterface();
+
+                while (true)
+                {
+                    Console.Write("HostCLI# ");
+                    command = Console.ReadLine();
+                    cli.RunCommand(command);
+                }
+                //if (args.Length != 0)
+                //{
+                //    Host host = new Host(args[0]);
+                //    host.ManagementRequest();
+                //    Console.ReadLine();
+                //    if (host.getName().Equals("Host1")) host.SendPacket("Host2", "test test test tset");
+                //    Console.ReadLine();
+                //    return;
+                //}
+
+                //    return;
+                //}
+                //Host host1 = new Host("Host1");
+                //host1.ManagementRequest();
+                //host1.ShowRoutingLines();
+
+                //Host host2 = new Host("Host2");
+                //host2.ManagementRequest();
+                //host2.ShowRoutingLines();
+
+
+                //UDPSocket s1 = new UDPSocket();
+                //UDPSocket s2 = new UDPSocket();
+
+                //host1.SetReceivingManagementSocket(1);
+                //host1.SetSendingManagementSocket(100);
+                //host1.ManagementRequest();
+                ////Host host2 = new Host("host2");
+                ////s1.Client("127.0.0.1", 1, host1);
+                ////s2.Server("127.0.0.1", 100, host1);
+                //host1.ParseLocalConfig();
+                //Console.ReadLine();
+                //Console.ReadKey();
+                //s1.Send("!@#$%^&*({}:@#>%!}{!%}{!$:%>#$:!}$%!#:$>");
+                //Console.ReadLine();
+                //string userInput;
+                //UDPSocket clientSocket = new UDPSocket();
+                //Host host1 = new Host("host1", 26999, 29002);
+                ////Host host2 = new Host("host2", 1000, 10001);
+                //MPLSLine mpls1 = new MPLSLine("host3", 1);
+                //host1.AddRoutingLineMPLS(mpls1);
+                //NHLFELine nhlfe1 = new NHLFELine(1, 17, 0);
+                //ILMLine ilm1 = new ILMLine(31, "host1");
+                //host1.AddNHLFELine(nhlfe1);
+                //host1.AddILMLIne(ilm1);
+                //clientSocket.Client("127.0.0.1", 27001, host1);
+                //host1.SendPacket("host3", "wiadomosc testowa");
+                //Console.ReadLine();
+                //while (true)
+                //{
+                //    string command;
+                //    String[] proccessedCommand;
+                //    Console.Write(host1.getName() + "> ");
+                //    command = Console.ReadLine();
+                //    if(command == "?")
+                //    {
+                //        BasicTerminal.GetHelp();
+                //    }
+                //    else
+                //    {
+                //        proccessedCommand = command.Split(' ');
+                //        if(proccessedCommand.Length == 3)
+                //        {
+                //            if(proccessedCommand[0] == "send")
+                //            clientSocket.Send(command);
+
+                //        }
+                //    }
+                //    //clientSocket.Send(msg);
+                //}
             }
 
-            //UDPSocket s1 = new UDPSocket();
-            //UDPSocket s2 = new UDPSocket();
-
-            //host1.SetReceivingManagementSocket(1);
-            //host1.SetSendingManagementSocket(100);
-            //host1.ManagementRequest();
-            ////Host host2 = new Host("host2");
-            ////s1.Client("127.0.0.1", 1, host1);
-            ////s2.Server("127.0.0.1", 100, host1);
-            //host1.ParseLocalConfig();
-            //Console.ReadLine();
-            //Console.ReadKey();
-            //s1.Send("!@#$%^&*({}:@#>%!}{!%}{!$:%>#$:!}$%!#:$>");
-            //Console.ReadLine();
-            //string userInput;
-            //UDPSocket clientSocket = new UDPSocket();
-            //Host host1 = new Host("host1", 26999, 29002);
-            ////Host host2 = new Host("host2", 1000, 10001);
-            //MPLSLine mpls1 = new MPLSLine("host3", 1);
-            //host1.AddRoutingLineMPLS(mpls1);
-            //NHLFELine nhlfe1 = new NHLFELine(1, 17, 0);
-            //ILMLine ilm1 = new ILMLine(31, "host1");
-            //host1.AddNHLFELine(nhlfe1);
-            //host1.AddILMLIne(ilm1);
-            //clientSocket.Client("127.0.0.1", 27001, host1);
-            //host1.SendPacket("host3", "wiadomosc testowa");
-            //Console.ReadLine();
-            //while (true)
-            //{
-            //    string command;
-            //    String[] proccessedCommand;
-            //    Console.Write(host1.getName() + "> ");
-            //    command = Console.ReadLine();
-            //    if(command == "?")
-            //    {
-            //        BasicTerminal.GetHelp();
-            //    }
-            //    else
-            //    {
-            //        proccessedCommand = command.Split(' ');
-            //        if(proccessedCommand.Length == 3)
-            //        {
-            //            if(proccessedCommand[0] == "send")
-            //            clientSocket.Send(command);
-
-            //        }
-            //    }
-            //    //clientSocket.Send(msg);
-            //}
         }
+
     }
 }
